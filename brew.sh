98fc5639923c07b49054e72749c1570d9db446ec
eriksf <erik@jcvi.org>
2016-04-19 15:38:56 -0400

    added installed packages to brew.sh

--- brew.sh
+++ brew.sh
@@ -3,7 +3,7 @@
 # Install command-line tools using Homebrew.
 
 # Make sure we’re using the latest Homebrew.
-brew update
+brew -v update
 
 # Upgrade any already-installed formulae.
 brew upgrade --all
@@ -24,77 +24,63 @@ brew install gnu-sed --with-default-names
 # running `chsh`.
 brew install bash
 brew tap homebrew/versions
+brew install bash-completion
 brew install bash-completion2
 
 # Install `wget` with IRI support.
 brew install wget --with-iri
 
-# Install RingoJS and Narwhal.
-# Note that the order in which these are installed is important;
-# see http://git.io/brew-narwhal-ringo.
-brew install ringojs
-brew install narwhal
+# tap some more
+brew tap homebrew/apache
+brew tap homebrew/core
+brew tap homebrew/dupes
+brew tap homebrew/php
+brew tap homebrew/science
 
 # Install more recent versions of some OS X tools.
 brew install vim --override-system-vi
 brew install homebrew/dupes/grep
 brew install homebrew/dupes/openssh
 brew install homebrew/dupes/screen
-brew install homebrew/php/php56 --with-gmp
-
-# Install font tools.
-brew tap bramstein/webfonttools
-brew install sfnt2woff
-brew install sfnt2woff-zopfli
-brew install woff2
-
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
 
 # Install other useful binaries.
-brew install ack
-brew install dark-mode
-#brew install exiv2
+brew install ant
+brew install autojump
+brew install boost
+brew install curl
+brew install enscript
+brew install ffmpeg --with-faac --with-ffplay --with-fontconfig --with-freetype --with-libass --with-libvorbis --with-tools --with-x265
+brew install homebrew/science/genometools --with-hmmer
 brew install git
-brew install git-lfs
-brew install imagemagick --with-webp
-brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
-brew install rename
-brew install rhino
-brew install speedtest_cli
+brew install graphviz
+brew install htop-osx
+brew install homebrew/apache/httpd22
+brew install httpie
+brew install hub
+brew install imap-uw
+brew install jq
+brew install libvo-aacenc
+brew install maven
+brew install mobile-shell
+brew install mysql
+brew install ncftp
+brew install nginx
+brew install nvm
+brew install perl
+brew install homebrew/php/php56 --with-debug --with-homebrew-apxs --with-homebrew-curl --with-postgresql
+brew install postgresql --with-python
+brew install pyside
+brew install python
+brew install rbenv
+brew install redis
+brew install rust
+brew install sdl
+brew install shellcheck
 brew install ssh-copy-id
-brew install testssl
-brew install tree
-brew install vbindiff
-brew install webkit2png
-brew install zopfli
+brew install subversion
+brew install tmux
+brew install homebrew/versions/tomcat7
+brew install watchman
 
 # Remove outdated versions from the cellar.
 brew cleanup
